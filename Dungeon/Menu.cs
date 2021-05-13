fd5992f70f589eb9b59bc9c3f853b7fb23fc014e
Rajesh <zechsnox@gmail.com>
2021-05-13 13:42:07 +0300

    Remove new Races() from player creation

--- Dungeon/Menu.cs
+++ Dungeon/Menu.cs
@@ -18,10 +18,9 @@ namespace Dungeon
 
         public void Player()
         {
-            Races Race = new Races();
-            foreach (var race in Race.GetPlayableRaces())
+            foreach (var race in Races.GetPlayableRaces())
             {
-                Console.WriteLine(Race.RaceKindToString(race));
+                Console.WriteLine(Races.RaceKindToString(race));
             }
             while (true)
             {
@@ -29,34 +28,34 @@ namespace Dungeon
                 Races.RacialModifiers stats;
                 switch (Input) {
                     case "HUMAN":
-                        stats = Race.GetRacialModifier(RaceKind.Human);
+                        stats = Races.GetRacialModifier(RaceKind.Human);
                         break;
                     case "HALF-ELF":
-                        stats = Race.GetRacialModifier(RaceKind.HalfElf);
+                        stats = Races.GetRacialModifier(RaceKind.HalfElf);
                         break;
                     case "ELF":
-                        stats = Race.GetRacialModifier(RaceKind.Elf);
+                        stats = Races.GetRacialModifier(RaceKind.Elf);
                         break;
                     case "ORC":
-                        stats = Race.GetRacialModifier(RaceKind.Orc);
+                        stats = Races.GetRacialModifier(RaceKind.Orc);
                         break;
                     case "GNOME":
-                        stats = Race.GetRacialModifier(RaceKind.Gnome);
+                        stats = Races.GetRacialModifier(RaceKind.Gnome);
                         break;
                     case "SKELETON":
-                        Race.GetRaceName(RaceKind.Skeleton);
-                        stats = Race.GetRacialModifier(RaceKind.Skeleton);
+                        Races.GetRaceName(RaceKind.Skeleton);
+                        stats = Races.GetRacialModifier(RaceKind.Skeleton);
                         break;
                     case "DWARF":
-                        Race.GetRaceName(RaceKind.Dwarf);
-                        stats = Race.GetRacialModifier(RaceKind.Dwarf);
+                        Races.GetRaceName(RaceKind.Dwarf);
+                        stats = Races.GetRacialModifier(RaceKind.Dwarf);
                         break;
                     case "GOBLIN":
-                        Race.GetRaceName(RaceKind.Goblin);
-                        stats = Race.GetRacialModifier(RaceKind.Goblin);
+                        Races.GetRaceName(RaceKind.Goblin);
+                        stats = Races.GetRacialModifier(RaceKind.Goblin);
                         break;
                     default:
-                        stats = Race.GetRacialModifier(RaceKind.Human);
+                        stats = Races.GetRacialModifier(RaceKind.Human);
                         break;
                 }
                 Console.WriteLine($"You have {stats.GetAttackModifier()} attack, {stats.GetArmorModifier()} armor and {stats.GetHealthModifier()} health");
