67e547268081691406700bbc9c3e580dff567b11
Rajesh <zechsnox@gmail.com>
2021-05-13 12:02:21 +0300

    Racial modifiers

--- Dungeon/Races.cs
+++ Dungeon/Races.cs
@@ -4,7 +4,6 @@ using System.Text;
 
 namespace Dungeon
 {
-
     public enum RaceKind {
         Human,
         HalfElf,
@@ -17,6 +16,17 @@ namespace Dungeon
     }
     class Races
     {
+        public struct RacialModifiers {
+            int health;
+            int armor;
+            int attack;
+
+            public RacialModifiers(int hp, int arm, int atk) {
+                this.health = hp;
+                this.armor = arm;
+                this.attack = atk;
+            }
+        }
         public String RaceKindToString(RaceKind race) {
             switch (race) {
                 case RaceKind.Human:
@@ -29,64 +39,24 @@ namespace Dungeon
                     return "Orc";
                 case RaceKind.Gnome:
                     return "Gnome";
+                case RaceKind.Dwarf:
+                    return "Dwarf";
+                case RaceKind.Skeleton:
+                    return "Skeleton";
+                case RaceKind.Goblin:
+                    return "Goblin";
                 default:
                     return "Undefined";
             }
         }
-        public void GetRacialModifier(RaceKind race) {
-            int health;
-            int armor;
-            int attack;
+        public RacialModifiers GetRacialModifier(RaceKind race) {
             switch (race) {
                 case RaceKind.Human:
-                    health = 100;
-                    armor = 2;
-                    attack = 2;
-                    Console.WriteLine($"You have {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
+                    return new RacialModifiers(100, 2, 2);
                 case RaceKind.HalfElf:
-                    health = 75;
-                    armor = 1;
-                    attack = 3;
-                    Console.WriteLine($"You have {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
-                case RaceKind.Elf:
-                    health = 85;
-                    armor = 0;
-                    attack = 4;
-                    Console.WriteLine($"You have {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
-                case RaceKind.Orc:
-                    health = 115;
-                    armor = 2;
-                    attack = 7;
-                    Console.WriteLine($"You have {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
-                case RaceKind.Gnome:
-                    health = 60;
-                    armor = 3;
-                    attack = 2;
-                    Console.WriteLine($"You have {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
-                case RaceKind.Skeleton:
-                    health = 75;
-                    attack = StaticRandom.Instance.Next(3, 5);
-                    armor = StaticRandom.Instance.Next(0, 12);
-                    Console.WriteLine($"They have: {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
-                case RaceKind.Dwarf:
-                    health = 100;
-                    attack = StaticRandom.Instance.Next(2, 4);
-                    armor = StaticRandom.Instance.Next(1, 3);
-                    Console.WriteLine($"They have: {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
-                case RaceKind.Goblin:
-                    health = 50;
-                    attack = StaticRandom.Instance.Next(1, 6);
-                    armor = StaticRandom.Instance.Next(0, 2);
-                    Console.WriteLine($"They have: {health} health points, {attack} attack damage and {armor} armor points!");
-                    break;
-
+                    return new RacialModifiers(75, 1, 3);
+                default:
+                    return new RacialModifiers(0,0,0);
             }
         }
         public void GetRaceName(RaceKind name)
@@ -95,18 +65,15 @@ namespace Dungeon
             {
                 case RaceKind.Skeleton:
                     String[] SkeletonBaseName = { "Indiana Bones", "Lazy Bonez", "Edward", "Dee Ceased", "Leboneski" };
-                    int IndexSkeleton = StaticRandom.Instance.Next(SkeletonBaseName.Length);
-                    Console.WriteLine($"{SkeletonBaseName[IndexSkeleton]} the {RaceKind.Skeleton} has appeared! ");
+                    Console.WriteLine($"{SkeletonBaseName[StaticRandom.Instance.Next(SkeletonBaseName.Length)]} the {RaceKind.Skeleton} has appeared! ");
                     break;
                 case RaceKind.Dwarf:
                     String[] DwarfBaseName = { "Nodreat", "Kisdras", "Reistoud", "Brundrek", "Thebaeck" };
-                    int IndexDwarf = StaticRandom.Instance.Next(DwarfBaseName.Length);
-                    Console.WriteLine($"{DwarfBaseName[IndexDwarf]} the {RaceKind.Dwarf} has appeared! ");
+                    Console.WriteLine($"{DwarfBaseName[StaticRandom.Instance.Next(DwarfBaseName.Length)]} the {RaceKind.Dwarf} has appeared! ");
                     break;
                 case RaceKind.Goblin:
                     String[] GoblinBaseName = { "Talk", "Zyskasb", "Zearark", "Srazz", "Klimirm" };
-                    int IndexGoblin = StaticRandom.Instance.Next(GoblinBaseName.Length);
-                    Console.WriteLine($"{GoblinBaseName[IndexGoblin]} the {RaceKind.Goblin} has appeared! ");
+                    Console.WriteLine($"{GoblinBaseName[StaticRandom.Instance.Next(GoblinBaseName.Length)]} the {RaceKind.Goblin} has appeared! ");
                     break;
             }
                 
