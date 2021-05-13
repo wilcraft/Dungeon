d3c8d83d63f500b9bce2e5f9cdcb95565469971d
wilcraft <wilcraftprods@gmail.com>
2021-05-13 13:46:59 +0300

    Commands

--- Dungeon/Commands.cs
+++ Dungeon/Commands.cs
@@ -8,5 +8,43 @@ namespace Dungeon
 {
     class Commands
     {
+        public enum CommandKind
+        {
+            Walk,
+            Sit,
+            Sleep
+        }
+        public String Action(CommandKind command)
+        {
+            switch (command)
+            {
+                case CommandKind.Walk:
+                    return "You walk thru the forest";
+                case CommandKind.Sit:
+                    return "You sit idly for a minute";
+                case CommandKind.Sleep:
+                    return "You sleep the day away";
+                default:
+                    return "Undefined";
+            }
+        }
+        public CommandKind StringtoAction(string move)
+        {
+            switch (move.ToUpper())
+            {
+                case "WALK":
+                    return CommandKind.Walk;
+                case "SIT":
+                    return CommandKind.Sit;
+                case "SLEEP":
+                    return CommandKind.Sleep;
+                default:
+                    return CommandKind.Walk;
+            }
+        }
+        public CommandKind[] GetUsableCommands()
+        {
+            return new CommandKind[] { CommandKind.Sit, CommandKind.Sleep, CommandKind.Walk};
+        }
     }
 }
--- Dungeon/Menu.cs
+++ Dungeon/Menu.cs
@@ -8,14 +8,12 @@ namespace Dungeon
     {
         public void BeginCreation()
         {
-            Menu Player = new Menu();
             Console.WriteLine("Would you like to create a character? Y/N");
             var Input = Console.ReadLine().ToUpper();
             var character = Input == "Y" ? "Select a race:" : Input == "N" ? "Farewell." : "";
             Console.WriteLine(character);
-            Player.Player();
+            Player();
         }
-
         public void Player()
         {
             Races Race = new Races();
@@ -23,48 +21,65 @@ namespace Dungeon
             {
                 Console.WriteLine(Race.RaceKindToString(race));
             }
-            while (true)
+            Selectrace();
+        }
+        public void Selectrace()
+        {
+            Races Race = new Races();
+            String Input = Console.ReadLine().ToUpper();
+            Races.RacialModifiers stats;
+            switch (Input)
             {
-                String Input = Console.ReadLine().ToUpper();
-                Races.RacialModifiers stats;
-                switch (Input) {
-                    case "HUMAN":
-                        stats = Race.GetRacialModifier(RaceKind.Human);
-                        break;
-                    case "HALF-ELF":
-                        stats = Race.GetRacialModifier(RaceKind.HalfElf);
-                        break;
-                    case "ELF":
-                        stats = Race.GetRacialModifier(RaceKind.Elf);
-                        break;
-                    case "ORC":
-                        stats = Race.GetRacialModifier(RaceKind.Orc);
-                        break;
-                    case "GNOME":
-                        stats = Race.GetRacialModifier(RaceKind.Gnome);
-                        break;
-                    case "SKELETON":
-                        Race.GetRaceName(RaceKind.Skeleton);
-                        stats = Race.GetRacialModifier(RaceKind.Skeleton);
-                        break;
-                    case "DWARF":
-                        Race.GetRaceName(RaceKind.Dwarf);
-                        stats = Race.GetRacialModifier(RaceKind.Dwarf);
-                        break;
-                    case "GOBLIN":
-                        Race.GetRaceName(RaceKind.Goblin);
-                        stats = Race.GetRacialModifier(RaceKind.Goblin);
-                        break;
-                    default:
-                        stats = Race.GetRacialModifier(RaceKind.Human);
-                        break;
+                case "HUMAN":
+                    stats = Race.GetRacialModifier(RaceKind.Human);
+                    break;
+                case "HALF-ELF":
+                    stats = Race.GetRacialModifier(RaceKind.HalfElf);
+                    break;
+                case "ELF":
+                    stats = Race.GetRacialModifier(RaceKind.Elf);
+                    break;
+                case "ORC":
+                    stats = Race.GetRacialModifier(RaceKind.Orc);
+                    break;
+                case "GNOME":
+                    stats = Race.GetRacialModifier(RaceKind.Gnome);
+                    break;
+                case "SKELETON":
+                    Race.GetRaceName(RaceKind.Skeleton);
+                    stats = Race.GetRacialModifier(RaceKind.Skeleton);
+                    break;
+                case "DWARF":
+                    Race.GetRaceName(RaceKind.Dwarf);
+                    stats = Race.GetRacialModifier(RaceKind.Dwarf);
+                    break;
+                case "GOBLIN":
+                    Race.GetRaceName(RaceKind.Goblin);
+                    stats = Race.GetRacialModifier(RaceKind.Goblin);
+                    break;
+                default:
+                    stats = Race.GetRacialModifier(RaceKind.Human);
+                    break;
+            }
+            Console.WriteLine($"You have {stats.GetAttackModifier()} attack, {stats.GetArmorModifier()} armor and {stats.GetHealthModifier()} health");
+            Console.WriteLine("Your Adventure begins in the creepy forest");
+            Adventure();
+        }
+        public void Adventure()
+        {
+            Console.WriteLine("What do you wish to do?");
+            Commands Usable = new Commands();
+            foreach (var usableaction in Usable.GetUsableCommands)
+            {
+                {
+                    Console.WriteLine();
                 }
-                Console.WriteLine($"You have {stats.GetAttackModifier()} attack, {stats.GetArmorModifier()} armor and {stats.GetHealthModifier()} health");
             }
         }
         public void Actions()
         {
-
+            Commands command = new Commands();
+            command.Action(command.StringtoAction(Console.ReadLine()));
         }
     }
 }
