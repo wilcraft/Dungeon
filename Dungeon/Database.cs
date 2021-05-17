767e9b1506f1ab4d7af4d42ba1b04b6ac59e31e4
Rajesh <zechsnox@gmail.com>
2021-05-17 11:46:52 +0300

    DB class

---
+++ Dungeon/Database.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using Dapper;
+
+namespace Dungeon
+{
+    public static class Database
+    {
+        private static readonly string ConString = "Data Source=./db.db;Version=3";
+        public static List<Room> LoadRooms()
+        {
+            using IDbConnection cnn = new SQLiteConnection(ConString);
+            var output = cnn.Query<Room>("select * from Rooms", new DynamicParameters());
+            return output.ToList();
+        }
+    }
+}
