0ecac0baa30de5344047a0eccc7f170c4417dc8f
Rajesh <zechsnox@gmail.com>
2021-05-17 11:46:28 +0300

    Parameterless Room constructor

--- Dungeon/Room.cs
+++ Dungeon/Room.cs
@@ -1,11 +1,11 @@
+using System.Data;
+using System.Data.Sql;
+
 namespace Dungeon {
-    class Room {
+    public class Room {
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
 
-        public Room(string shortd, string longd) {
-            this.ShortDescription = shortd;
-            this.LongDescription = longd;
-        }
+        public Room() { }
     }
 }
