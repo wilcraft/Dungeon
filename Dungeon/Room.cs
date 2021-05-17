3b1a3a8e9d75d4e4c0b236bdf1a76a7ae0e8b490
Rajesh <zechsnox@gmail.com>
2021-05-18 00:16:16 +0300

    Added RoomId member (uint32)

--- Dungeon/Room.cs
+++ Dungeon/Room.cs
@@ -1,8 +1,11 @@
 using System.Data;
 using System.Data.Sql;
 
+using RoomId = System.UInt32;
+
 namespace Dungeon {
     public class Room {
+        public RoomId Id { get; set; }
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
 
